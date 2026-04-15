a07bb96516b4e18637811a6218205d14a315f3dc
Afomia Solomon <afomia.solomonm@gmail.com>
2026-04-15 15:40:55 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import math
 
 st.set_page_config(page_title="My Calculator", page_icon="🔢")
 def calculate(num1, num2, operator):
@@ -15,10 +16,16 @@ def calculate(num1, num2, operator):
       return'Cannot divide by zero.'
   if operator == 'Power':
     return num1**num2
+  if operator == 'Square Root':
+    if num1 < 0:
+      return math.sqrt(num1)
+    else: 
+      return 'Error: Cannot square root a negative number.'  
   else:
     return 'Invalid Operator'
 
 st.title('Calculator')
+col1, col2 = st.columns(2)
 with col1:
     num1 = st.number_input('Enter the first number.')
 with col2:    
