6f682dd4b0ff32ff45892aa56c438c0631edec34
Afomia Solomon <afomia.solomonm@gmail.com>
2026-04-15 14:30:40 +0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+def calculate(num1, num2, operator):
+  if operator == '+':
+    return   num1 + num2
+  if operator == '-':
+    return num1 - num2   
+  if operator == '*':
+    return num1*num2
+  if operator == '/':
+    return num1/num2   
+  else:
+    return 'Invalid Operator'
+
+st.title('Calculator')  
